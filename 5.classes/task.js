b6003f9ef1236ac748312876c6bec9352d628267
Mishima <modernden4@gmail.com>
2023-09-29 13:48:23 +0300

    классы

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -30,31 +30,31 @@ class Magazine extends PrintEditionItem {
 }
 
 class Book extends PrintEditionItem {
-	constructor(name, releaseDate, pagesCount,author) {
-		super(name, releaseDate, pagesCount,author);
+	constructor(author,name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
 		this.author = author;
 		this.type = "book";
 	}
 }
 class NovelBook extends Book {
-	constructor(name, releaseDate, pagesCount, author) {
-		super(name, releaseDate, pagesCount, author);;
+	constructor(author,name, releaseDate, pagesCount) {
+		super(author,name, releaseDate, pagesCount);;
 		this.type = "novel"
 	}
 
 }
 
 class FantasticBook extends Book {
-	constructor(name, releaseDate, pagesCount, author) {
-		super(name, releaseDate, pagesCount, author);
+	constructor(author,name, releaseDate, pagesCount) {
+		super(author,name, releaseDate, pagesCount);
 		this.type = "fantastic";
 	}
 
 }
 
 class DetectiveBook extends Book {
-	constructor(name, releaseDate, pagesCount, author) {
-		super(name, releaseDate, pagesCount, author);
+	constructor(author,name, releaseDate, pagesCount) {
+		super(author,name, releaseDate, pagesCount);
 		this.type = "detective";
 	}
 
